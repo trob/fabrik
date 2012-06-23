211629c442ccd14ff7a29a6ecd13a1cfbfb6e80b
trob <beate@.(none)>
2012-06-23 12:54:44 +0200

    dompf

--- libraries/dompdf/dompdf_config.custom.inc.php
+++ libraries/dompdf/dompdf_config.custom.inc.php
@@ -4,12 +4,12 @@
 //define("DOMPDF_UNICODE_ENABLED", false);
 //define("DOMPDF_PDF_BACKEND", "PDFLib");
 //define("DOMPDF_DEFAULT_MEDIA_TYPE", "print");
-define("DOMPDF_DEFAULT_PAPER_SIZE", "A4");
+//define("DOMPDF_DEFAULT_PAPER_SIZE", "A4");
 //define("DOMPDF_DEFAULT_FONT", "serif");
 //define("DOMPDF_DPI", 72);
 //define("DOMPDF_ENABLE_PHP", true);
 //define("DOMPDF_ENABLE_REMOTE", true);
-define("DOMPDF_ENABLE_CSS_FLOAT", true);
+//define("DOMPDF_ENABLE_CSS_FLOAT", true);
 //define("DOMPDF_ENABLE_JAVASCRIPT", false);
 //define("DEBUGPNG", true);
 //define("DEBUGKEEPTEMP", true);
--- libraries/dompdf/dompdf_config.inc.php
+++ libraries/dompdf/dompdf_config.inc.php
@@ -199,7 +199,7 @@ def("DOMPDF_DEFAULT_MEDIA_TYPE", "screen");
  *
  * @see CPDF_Adapter::PAPER_SIZES for valid sizes
  */
-def("DOMPDF_DEFAULT_PAPER_SIZE", "letter");
+def("DOMPDF_DEFAULT_PAPER_SIZE", "a4");
 
 /**
  * The default font family
@@ -291,7 +291,7 @@ def("DOMPDF_ENABLE_REMOTE", true);
  * The debug output log
  * @var string
  */
-def("DOMPDF_LOG_OUTPUT_FILE", DOMPDF_FONT_DIR."log.htm");
+def("DOMPDF_LOG_OUTPUT_FILE", DOMPDF_FONT_DIR."logaaa.htm");
 
 /**
  * A ratio applied to the fonts height to be more like browsers' line height
