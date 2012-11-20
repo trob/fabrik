6051ef66d6062706e325d3f53700e8d1427af38f
trob <beate@.(none)>
2012-11-20 08:51:31 +0100

    tabs/default_group with left col

--- components/com_fabrik/views/form/tmpl/tabs/default_group.php
+++ components/com_fabrik/views/form/tmpl/tabs/default_group.php
@@ -13,8 +13,10 @@
 <?php foreach ( $this->elements as $element) {
 	?>
 	<li <?php echo @$element->column;?> class="<?php echo $element->containerClass;?>">
+	<div class="leftCol">
 		<?php echo $element->label;?>
 		<?php echo $element->errorTag; ?>
+	</div>		
 		<div class="fabrikElement">
 			<?php echo $element->element;?>
 
